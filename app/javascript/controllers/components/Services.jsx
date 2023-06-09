c530b1805ac3c366a77200aa1681e44937cfc4e9
Declan Noetzelman <declan@noetzelman.com>
2023-06-09 17:09:25 -0400

    services page finished and testimonials started

--- app/javascript/controllers/components/Services.jsx
+++ app/javascript/controllers/components/Services.jsx
@@ -3,7 +3,59 @@ import Navbar from './Navbar'
 
 function Services() {
   return (
-    <Navbar />
+    <div id="services">
+      <Navbar />
+      <div id="services-container">
+        <p id="services-head">Services</p>
+        <p id="services-tagline">
+          All products used at Tramway Dog Grooming are all natural, certified gluten free, non-irritating and paraben and sulfate free. We ensure that your dogs don’t just look their best but feel their best as well!
+        </p>
+        <div className="services-section-container">
+          <div className="services-img-container">
+            <img className="services-img" src="https://i.imgur.com/NoCWm6M.jpg"/>
+          </div>
+          <div className="services-text-container">
+            <p className="services-name">Bath and Brush</p>
+            <p className="services-text">
+              Our bath packages are deep cleaning. They remove oil, grease, dirt and stains from the coat. We use products that are non irritating to the skin for both animals and humans. Formulated with natural ingredients to condition & soothe the skin and leave the coat manageable with a quality finish!
+            </p>
+          </div>
+        </div>
+        <div className="services-section-container">
+          <div className="services-img-container">
+            <img className="services-img" src="https://i.imgur.com/McKtkUa.jpg"/>
+          </div>
+          <div className="services-text-container">
+            <p className="services-name">Deshed Treatment</p>
+            <p className="services-text">
+              Our Natural De-Shedding Package is a premium solution for removing loose hair and undercoat. This powerful formula will detangle, release and remove loose hair and undercoat reducing stress on the pet from time spent brushing and blow drying. This premium hypoallergenic formulation will leave all coat types conditioned, moisturized and silky smooth with reduction of shedding. Vitamin and protein enriched as well as amino acids to strengthen the hair shaft, rejuvenate and promote healthy skin!
+            </p>
+          </div>
+        </div>
+        <div className="services-section-container">
+          <div className="services-img-container">
+            <img className="services-img" src="https://i.imgur.com/5ZQ3Z4q.jpg"/>
+          </div>
+          <div className="services-text-container">
+            <p className="services-name">Tidy Groom</p>
+            <p className="services-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+        <div className="services-section-container">
+          <div className="services-img-container">
+            <img className="services-img" src="https://i.imgur.com/5ZQ3Z4q.jpg"/>
+          </div>
+          <div className="services-text-container">
+            <p className="services-name">Full Service Groom</p>
+            <p className="services-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/javascript/controllers/components/Testimonials.jsx
+++ app/javascript/controllers/components/Testimonials.jsx
@@ -3,7 +3,42 @@ import Navbar from './Navbar'
 
 function Testimonials() {
   return (
-    <Navbar />
+    <div id="testimonials">
+      <Navbar />
+      <div id="testimonials-container">
+        <p id="testimonials-head">Don't just take it from us, hear it from our happy clients!</p>
+        <div id="testimonials-section-container">
+          <p className="testimonials-author">Dixie Barnes</p>
+          <p className="testimonials-text">
+            Tramway Dog Grooming is the best around! My fur babies LOVE them! They make grooming a great experience for them. I have one nervous and one outgoing little girl and they both enjoy their spa day here! Jackey, Ian, Mackenzie and Melanie are amazing! It’s hard to find people you trust with your pets but there is no issue here as they’re treated as their own pets! The appointments are quick which is convenient but also very efficient! I love how they take the time to take pictures of all of the babies that come through their doors. My girls come home smelling good and looking as cute as ever! Highly recommend!
+          </p>
+        </div>
+        <div id="testimonials-section-container">
+          <p className="testimonials-author">Rhiannon Von Nunziato</p>
+          <p className="testimonials-text">
+            Love Tramway Dog Grooming so much! They are so attentive and thorough with my two kids ❤️ Both my doggos are big time shedders & visits to Jackey and Ian save my vacuum cleaner and my sanity! I always really appreciate that Jackey takes the time to offers suggestions for products to use at home in between visits- it’s so obvious that they truly care about their clients! Highly recommend!
+          </p>
+        </div>
+        <div id="testimonials-section-container">
+          <p className="testimonials-author">Abby Lamm</p>
+          <p className="testimonials-text">
+            This was my dogs first time at the groomers and they did great. They made him feel so comfortable and he looks amazing! Highly recommend 😊
+          </p>
+        </div>
+        <div id="testimonials-section-container">
+          <p className="testimonials-author">Yazmin Del Castillo</p>
+          <p className="testimonials-text">
+            Literally a great experience! I would love to bring my baby back again! Such an amazing team and so friendly! I will definitely be booking with Tramway Dog Grooming from now on! Thank you for treating my baby so wonderful!
+          </p>
+        </div>
+        <div id="testimonials-section-container">
+          <p className="testimonials-author">Alyssa Clayton</p>
+          <p className="testimonials-text">
+            Gibby is a bull mastiff 'puppy' whose hobbies include chewing sticks, drooling, going for walks and SHEDDING. Two expensive de-shedding treatments elsewhere were making it worse, it seemed. We found Tramway Dog Grooming where the owners were some of the nicest people Ive ever met and they were able to see Gibby the day we called! Gibby came home so clean, smelling good and NOT shedding! He was such a happy boy and even got a goody bag 😁 We have found his permanent groomer!
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
